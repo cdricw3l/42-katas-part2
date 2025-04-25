7c7723a9cf2cbb8273be564e458b42bcced81278
ast <cdricw3l@gmail.com>
2025-04-25 22:13:29 +0200

    philo/init_network_and_clean/2025-04-25T22:13:29+02:00

--- philosopher/unit_test/tst_main.c
+++ philosopher/unit_test/tst_main.c
@@ -6,7 +6,7 @@
 /*   By: ast <ast@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 11:22:41 by cbouhadr          #+#    #+#             */
-/*   Updated: 2025/04/25 21:36:56 by ast              ###   ########.fr       */
+/*   Updated: 2025/04/25 22:13:23 by ast              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -17,12 +17,13 @@ int	main(void)
     //assert(tst_ft_init_arr_arg() == 1);
     //assert(tst_init_fork_and_pen() == 1);
     // assert(tst_init_network() == 1);
+    //assert(tst_timestamp() == 1);
+    assert(tst_init_philos() == 1);
+    assert(tst_init_network() == 1);
+    
     // assert(tst_run_thread_monitor() == 1);
     // assert(tst_run_thread_philo() == 1);
     // assert(tst_run_thread_simulation() == 1);
-    //assert(tst_timestamp() == 1);
-    assert(tst_init_philos() == 1);
-
     //assert(tst_tempo() == 1);
     TEST_SUCCES;
 	return(0);

0b418aa98b53f189ac1de1c8d1bf9e18309a26c1
cb <cdricw3l@gmail.com>
2025-05-29 05:24:13 +0200

    push_swap/Jeu_29_mai_2025_05:24:13_CEST

--- philosopher/test/main.c
+++ philosopher/test/main.c
@@ -6,7 +6,7 @@
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 06:57:49 by cw3l              #+#    #+#             */
-/*   Updated: 2025/05/28 07:08:42 by cw3l             ###   ########.fr       */
+/*   Updated: 2025/05/28 20:25:52 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,26 +35,30 @@ void *routine_2(void *arg)
     i = 0;
     while (i < 10)
     {
-        printf("thred 1 counter : %d\n", i);
+        printf("thred 2 counter : %d\n", i);
         i++;
     }
-    return(NULL);
+    return(arg);
 }
 
 
 int main(void)
 {
 
-    pthread_t t1;
-    pthread_t t2;
+    pthread_t t1 = NULL;
+    pthread_t t2 = NULL;
 
     
     // params : thrad,  
-    if(pthread_create(&t1,NULL,routine_1,NULL) || pthread_create(&t2,NULL,routine_1,NULL))
+    if(pthread_create(&t1,NULL,routine_1,NULL))
     {
         printf("Erreur dans le lancement des thread\n");
     }
-    if(pthread_join(t1,NULL))
+    if( pthread_create(&t2,NULL,routine_2,NULL))
+    {
+        printf("Erreur dans le lancement des thread\n");
+    }
+    if(pthread_join(t1, NULL) || pthread_join(t2, NULL))
         printf("Erreur dans la jonction des thread\n");
     return(0);
 }
